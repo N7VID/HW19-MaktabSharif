6620c3ad094fdf20fded43a14f18960ebf7094ba
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-10 18:59:01 +0330

    feat: homepage route

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-
-function App() {
-  return <div className="bg-red-300">HI</div>;
-}
-
-export default App;
---
+++ src/pages/home-page/HomePage.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <div className="bg-red-300">HI</div>;
+}
