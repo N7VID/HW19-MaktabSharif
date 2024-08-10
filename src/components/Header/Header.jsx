cded250baf7df4209558fc7e08fe790de7cc05cd
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-10 19:51:04 +0330

    feat: base style Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center max-w-[1450px] mx-auto py-2">
+      <img src="/MAKTAB2.png" alt="Maktab Sharif Icon" className="w-14" />
+    </div>
+  );
+}
