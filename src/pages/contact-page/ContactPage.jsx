b03645d826ae7412f46a4111f87060e7df931915
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-14 20:36:56 +0330

    feat: contactPage content

--- src/pages/contact-page/ContactPage.jsx
+++ src/pages/contact-page/ContactPage.jsx
@@ -1,3 +1,21 @@
 export default function ContactPage() {
-  return <div>ContactPage</div>;
+  return (
+    <div className="pt-[100px] flex justify-center items-center bg-mainBg min-h-screen cursor-default">
+      <div className="flex-grow flex flex-col px-20 gap-4">
+        <h2 className="text-4xl font-[900] text-[#333] text-left">{`<Contact MaktabSharif Team/>`}</h2>
+        <h3 className="text-2xl font-[800] text-[#333] text-left mb-4">
+          The largest programming family in the country
+        </h3>
+        <p className="w-[490px] text-balance mb-4">
+          Today, we are happy and satisfied with the 7-year effort of the school
+          that we had for the honor and motivation of these people's success,
+          and we step into the future days with a world of bigger dreams.
+        </p>
+        <div>{/* <Button variant={"secondary"}>View More</Button> */}</div>
+      </div>
+      <div className="w-[720px]">
+        <img src="/hero-contact.webp" alt="hero aboutPage" className="w-full" />
+      </div>
+    </div>
+  );
 }
